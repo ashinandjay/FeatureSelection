68c96fffc406fb326af92de7ce9d66135b0b8bd8
ashinandjay <40191792+ashinandjay@users.noreply.github.com>
2019-07-01 00:20:53 -0500

    Update Feature_Selection.py

--- Feature_Selection.py
+++ Feature_Selection.py
@@ -24,6 +24,10 @@ from scipy.special import digamma
 from math import log
 import numpy.random as nr
 from skrebate import ReliefF
+import numpy.matlib
+from sklearn.metrics.pairwise import rbf_kernel
+from numpy import linalg as LA
+from scipy.sparse import *
 
 # find the path
 Script_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
@@ -42,7 +46,7 @@ feature_o = pd.read_csv(feature,delim_whitespace=True,index_col=0,float_precisio
 label = sys.argv[4]
 label = pd.read_csv(label,delim_whitespace=True,index_col=0)
 
-# Feature selection methods:
+#- Feature selection methods ---------------------------------------------------------------------
 
 # Feature selection Lasso    
 def Lasso_selection(k,X,y):
@@ -62,7 +66,7 @@ def Elastic_selection(k,X,y):
 
 # Feature selection L1-SVM
 def L1SVM_selection(k,X,y):
-    lsvc = LinearSVC(penatly="l1",dual=False)
+    lsvc = LinearSVC(penalty="l1",dual=False)
     lsvcresult = RFE(lsvc,k).fit(X,y.values.ravel()).get_support(indices=True)
     lsvcresult = X[X.columns[lsvcresult]]
     lsvcresult.to_csv("L1SVM_out.csv")
@@ -267,8 +271,7 @@ def mrmr(X, y, **kwargs):
         F, J_CMI, MIfy = lcsi(X, y, gamma=0, function_name='MRMR', n_selected_features=n_selected_features)
     else:
         F, J_CMI, MIfy = lcsi(X, y, gamma=0, function_name='MRMR')
-    X_new=X[:,F]
-    return X_new
+    return F
 
 # Feature selection JMI
 def jmi(X, y, **kwargs):
@@ -277,8 +280,7 @@ def jmi(X, y, **kwargs):
         F, J_CMI, MIfy = lcsi(X, y, function_name='JMI', n_selected_features=n_selected_features)
     else:
         F, J_CMI, MIfy = lcsi(X, y, function_name='JMI')
-    X_new=X[:,F]
-    return X_new
+    return F
 
 # Feature selection MRMD
 def calcE(X,coli,colj):
@@ -337,18 +339,269 @@ def mrmd(X,y,n_selected_features=10):
     mrmd_order=sorted(mrmrValue,key=lambda x:x[1],reverse=True)  
     mrmd_order =[int(x[0]) for x in mrmd_order]
     mrmd_end=mrmd_order[:n_selected_features]
-    X_new=X[:,mrmd_end]
-    return X_new
+    return mrmd_end
 
 # Feature selection ReliefF
-def ReliefF(X,y,n):
+def ReliefF_Method(X,y,n):
     X=np.array(X)
     y=np.asarray(y)
     y = y[:, 0]
-    clf = ReliefF(n_features_to_select=3, n_neighbors=100)
+    clf = ReliefF(n_features_to_select=n, n_neighbors=100)
     Reresult = clf.fit_transform(X,y)
-    Reresult.to_csv("ReliefF_out.csv")
+    np.savetxt("ReliefF_out.csv", Reresult, delimiter=",")
     return None
+
+# Feature selection trace_ratio
+def trace_ratio(X, y, n_selected_features, **kwargs):
+    import construct_W
+    # if 'style' is not specified, use the fisher score way to built two affinity matrix
+    if 'style' not in kwargs.keys():
+        kwargs['style'] = 'fisher'
+    # get the way to build affinity matrix, 'fisher' or 'laplacian'
+    style = kwargs['style']
+    n_samples, n_features = X.shape
+
+    # if 'verbose' is not specified, do not output the value of objective function
+    if 'verbose' not in kwargs:
+        kwargs['verbose'] = False
+    verbose = kwargs['verbose']
+
+    if style is 'fisher':
+        kwargs_within = {"neighbor_mode": "supervised", "fisher_score": True, 'y': y}
+        # build within class and between class laplacian matrix L_w and L_b
+        W_within = construct_W.construct_W(X, **kwargs_within)
+        L_within = np.eye(n_samples) - W_within
+        L_tmp = np.eye(n_samples) - np.ones([n_samples, n_samples])/n_samples
+        L_between = L_within - L_tmp
+
+    if style is 'laplacian':
+        kwargs_within = {"metric": "euclidean", "neighbor_mode": "knn", "weight_mode": "heat_kernel", "k": 5, 't': 1}
+        # build within class and between class laplacian matrix L_w and L_b
+        W_within = construct_W.construct_W(X, **kwargs_within)
+        D_within = np.diag(np.array(W_within.sum(1))[:, 0])
+        L_within = D_within - W_within
+        W_between = np.dot(np.dot(D_within, np.ones([n_samples, n_samples])), D_within)/np.sum(D_within)
+        D_between = np.diag(np.array(W_between.sum(1)))
+        L_between = D_between - W_between
+
+    # build X'*L_within*X and X'*L_between*X
+    L_within = (np.transpose(L_within) + L_within)/2
+    L_between = (np.transpose(L_between) + L_between)/2
+    S_within = np.array(np.dot(np.dot(np.transpose(X), L_within), X))
+    S_between = np.array(np.dot(np.dot(np.transpose(X), L_between), X))
+
+    # reflect the within-class or local affinity relationship encoded on graph, Sw = X*Lw*X'
+    S_within = (np.transpose(S_within) + S_within)/2
+    # reflect the between-class or global affinity relationship encoded on graph, Sb = X*Lb*X'
+    S_between = (np.transpose(S_between) + S_between)/2
+
+    # take the absolute values of diagonal
+    s_within = np.absolute(S_within.diagonal())
+    s_between = np.absolute(S_between.diagonal())
+    s_between[s_between == 0] = 1e-14  # this number if from authors' code
+
+    # preprocessing
+    fs_idx = np.argsort(np.divide(s_between, s_within), 0)[::-1]
+    k = np.sum(s_between[0:n_selected_features])/np.sum(s_within[0:n_selected_features])
+    s_within = s_within[fs_idx[0:n_selected_features]]
+    s_between = s_between[fs_idx[0:n_selected_features]]
+
+    # iterate util converge
+    count = 0
+    while True:
+        score = np.sort(s_between-k*s_within)[::-1]
+        I = np.argsort(s_between-k*s_within)[::-1]
+        idx = I[0:n_selected_features]
+        old_k = k
+        k = np.sum(s_between[idx])/np.sum(s_within[idx])
+        if verbose:
+            print('obj at iter {0}: {1}'.format(count+1, k))
+        count += 1
+        if abs(k - old_k) < 1e-3:
+            break
+
+    # get feature index, feature-level score and subset-level score
+    feature_idx = fs_idx[I]
+    feature_score = score
+    subset_score = k
+    return feature_idx, feature_score, subset_score
+
+# Feature selection gini index
+def gini_index(X, y):
+    n_samples, n_features = X.shape
+    # initialize gini_index for all features to be 0.5
+    gini = np.ones(n_features) * 0.5
+    # For i-th feature we define fi = x[:,i] ,v include all unique values in fi
+    for i in range(n_features):
+        v = np.unique(X[:, i])
+        for j in range(len(v)):
+            # left_y contains labels of instances whose i-th feature value is less than or equal to v[j]
+            left_y = y[X[:, i] <= v[j]]
+            # right_y contains labels of instances whose i-th feature value is larger than v[j]
+            right_y = y[X[:, i] > v[j]]
+            # gini_left is sum of square of probability of occurrence of v[i] in left_y
+            # gini_right is sum of square of probability of occurrence of v[i] in right_y
+            gini_left = 0
+            gini_right = 0
+            for k in range(np.min(y), np.max(y)+1):
+                if len(left_y) != 0:
+                    # t1_left is probability of occurrence of k in left_y
+                    t1_left = np.true_divide(len(left_y[left_y == k]), len(left_y))
+                    t2_left = np.power(t1_left, 2)
+                    gini_left += t2_left
+                if len(right_y) != 0:
+                    # t1_right is probability of occurrence of k in left_y
+                    t1_right = np.true_divide(len(right_y[right_y == k]), len(right_y))
+                    t2_right = np.power(t1_right, 2)
+                    gini_right += t2_right
+            gini_left = 1 - gini_left
+            gini_right = 1 - gini_right
+            # weighted average of len(left_y) and len(right_y)
+            t1_gini = (len(left_y) * gini_left + len(right_y) * gini_right)
+            # compute the gini_index for the i-th feature
+            value = np.true_divide(t1_gini, len(y))
+            if value < gini[i]:
+                gini[i] = value
+    return gini
+
+def feature_ranking(W):
+    idx = np.argsort(W)
+    return idx
+
+# Feature selection SPEC
+def spec(X, **kwargs):
+    if 'style' not in kwargs:
+        kwargs['style'] = 0
+    if 'W' not in kwargs:
+        kwargs['W'] = rbf_kernel(X, gamma=1)
+
+    style = kwargs['style']
+    W = kwargs['W']
+    if type(W) is numpy.ndarray:
+        W = csc_matrix(W)
+    n_samples, n_features = X.shape
+    # build the degree matrix
+    X_sum = np.array(W.sum(axis=1))
+    D = np.zeros((n_samples, n_samples))
+    for i in range(n_samples):
+        D[i, i] = X_sum[i]
+    # build the laplacian matrix
+    L = D - W
+    d1 = np.power(np.array(W.sum(axis=1)), -0.5)
+    d1[np.isinf(d1)] = 0
+    d2 = np.power(np.array(W.sum(axis=1)), 0.5)
+    v = np.dot(np.diag(d2[:, 0]), np.ones(n_samples))
+    v = v/LA.norm(v)
+    # build the normalized laplacian matrix
+    L_hat = (np.matlib.repmat(d1, 1, n_samples)) * np.array(L) * np.matlib.repmat(np.transpose(d1), n_samples, 1)
+    # calculate and construct spectral information
+    s, U = np.linalg.eigh(L_hat)
+    s = np.flipud(s)
+    U = np.fliplr(U)
+    # begin to select features
+    w_fea = np.ones(n_features)*1000
+    for i in range(n_features):
+        f = X[:, i]
+        F_hat = np.dot(np.diag(d2[:, 0]), f)
+        l = LA.norm(F_hat)
+        if l < 100*np.spacing(1):
+            w_fea[i] = 1000
+            continue
+        else:
+            F_hat = F_hat/l
+        a = np.array(np.dot(np.transpose(F_hat), U))
+        a = np.multiply(a, a)
+        a = np.transpose(a)
+
+        # use f'Lf formulation
+        if style == -1:
+            w_fea[i] = np.sum(a * s)
+        # using all eigenvalues except the 1st
+        elif style == 0:
+            a1 = a[0:n_samples-1]
+            w_fea[i] = np.sum(a1 * s[0:n_samples-1])/(1-np.power(np.dot(np.transpose(F_hat), v), 2))
+        # use first k except the 1st
+        else:
+            a1 = a[n_samples-style:n_samples-1]
+            w_fea[i] = np.sum(a1 * (2-s[n_samples-style: n_samples-1]))
+
+    if style != -1 and style != 0:
+        w_fea[w_fea == 1000] = -1000
+    return w_fea
+
+def feature_ranking2(score, **kwargs):
+    if 'style' not in kwargs:
+        kwargs['style'] = 0
+    style = kwargs['style']
+
+    # if style = -1 or 0, ranking features in descending order, the higher the score, the more important the feature is
+    if style == -1 or style == 0:
+        idx = np.argsort(score, 0)
+        return idx[::-1]
+    # if style != -1 and 0, ranking features in ascending order, the lower the score, the more important the feature is
+    elif style != -1 and style != 0:
+        idx = np.argsort(score, 0)
+        return idx
+
+# Feature selection Fisher Score
+import construct_W
+def fisher_score(X, y):
+    # Construct weight matrix W in a fisherScore way
+    kwargs = {"neighbor_mode": "supervised", "fisher_score": True, 'y': y}
+    W = construct_W.construct_W(X, **kwargs)
+    # build the diagonal D matrix from affinity matrix W
+    D = np.array(W.sum(axis=1))
+    L = W
+    tmp = np.dot(np.transpose(D), X)
+    D = diags(np.transpose(D), [0])
+    Xt = np.transpose(X)
+    t1 = np.transpose(np.dot(Xt, D.todense()))
+    t2 = np.transpose(np.dot(Xt, L.todense()))
+    # compute the numerator of Lr
+    D_prime = np.sum(np.multiply(t1, X), 0) - np.multiply(tmp, tmp)/D.sum()
+    # compute the denominator of Lr
+    L_prime = np.sum(np.multiply(t2, X), 0) - np.multiply(tmp, tmp)/D.sum()
+    # avoid the denominator of Lr to be 0
+    D_prime[D_prime < 1e-12] = 10000
+    lap_score = 1 - np.array(np.multiply(L_prime, 1/D_prime))[0, :]
+    # compute fisher score from laplacian score, where fisher_score = 1/lap_score - 1
+    score = 1.0/lap_score - 1
+    return np.transpose(score)
+
+def feature_ranking3(score):
+    idx = np.argsort(score, 0)
+    return idx[::-1]
+
+# Feature selection t_score
+def t_score(X, y):
+    n_samples, n_features = X.shape
+    F = np.zeros(n_features)
+    c = np.unique(y)
+    if len(c) == 2:
+        for i in range(n_features):
+            f = X[:, i]
+            # class0 contains instances belonging to the first class
+            # class1 contains instances belonging to the second class
+            class0 = f[y == c[0]]
+            class1 = f[y == c[1]]
+            mean0 = np.mean(class0)
+            mean1 = np.mean(class1)
+            std0 = np.std(class0)
+            std1 = np.std(class1)
+            n0 = len(class0)
+            n1 = len(class1)
+            t = mean0 - mean1
+            t0 = np.true_divide(std0**2, n0)
+            t1 = np.true_divide(std1**2, n1)
+            F[i] = np.true_divide(t, (t0 + t1)**0.5)
+    else:
+        print('y should be guaranteed to a binary class vector')
+        exit(0)
+    return np.abs(F)
+
+def feature_ranking4(F):
+    idx = np.argsort(F)
+    return idx[::-1]
 
 # Feature selection IG
 def calProb(array):
@@ -420,7 +673,7 @@ def IG(encodings,labelfile,k):
     new_data=data[:,mask]
     return new_data
 
-# main function
+#- main function ----------------------------------------------------------------------
 
 if(option == "1"):
     # Feature selection Lasso
@@ -436,7 +689,7 @@ elif(option == "4"):
     Chi2_selection(selected_number,feature_o,label)
 elif(option == "5"):
     # Feature selection Pearson Correlation
-    cor_support, cor_feature = cor_selector(feature_o, label.values.ravel())
+    cor_support, cor_feature = cor_selector(selected_number,feature_o, label.values.ravel())
     PCresult = feature_o[cor_feature]
     PCresult.to_csv("PearsonCorrelation_out.csv")
 elif(option == "6"):
@@ -459,37 +712,79 @@ elif(option == "11"):
     X=np.array(feature_o)
     y=np.asarray(label)
     y = y[:, 0]
-    MRMRresult=mrmr(X,y,selected_number)
+    MRMRresult=mrmr(X,y,n_selected_features=selected_number)
+    MRMRresult = feature_o[feature_o.columns[MRMRresult]]
     MRMRresult.to_csv("MRMR_out.csv")
 elif(option == "12"):
     # Feature selection JMI
     X=np.array(feature_o)
     y=np.asarray(label)
     y = y[:, 0]
-    JMIresult=mrmd(X,y,selected_number)
+    JMIresult=jmi(X,y,n_selected_features=selected_number)
+    JMIresult = feature_o[feature_o.columns[JMIresult]]
     JMIresult.to_csv("JMI_out.csv")
 elif(option == "13"):
     # Feature selection MRMD
     X=np.array(feature_o)
     y=np.array(label)
-    MRMDresult=mrmd(X,y,selected_number)
+    y = y[:, 0]
+    MRMDresult=mrmd(X,y,n_selected_features=selected_number)
+    MRMDresult = feature_o[feature_o.columns[MRMDresult]]
     MRMDresult.to_csv("MRMD_out.csv")
 elif(option == "14"):
     # Feature selection ReliefF
-    ReliefF(feature_o,label,selected_number)
+    ReliefF_Method(feature_o,label,selected_number)
 elif(option == "15"):
     # Feature selection trace_ratio
+    X=np.array(feature_o)
+    y=np.array(label)
+    y=y[:,0]
+    idx, feature_score, subset_score = trace_ratio(X, y, selected_number, style='fisher')
+    traceresult = feature_o[feature_o.columns[idx]]
+    traceresult.to_csv("TRACE_RATIO_out.csv")
 elif(option == "16"):
     # Feature selection gini_index
+    X=np.array(feature_o)
+    y=np.asarray(label)
+    y = y[:, 0]
+    score = gini_index(X, y)
+    # rank features in descending order according to score
+    idx =feature_ranking(score)
+    giniresult = feature_o[feature_o.columns[idx[0:selected_number]]]
+    giniresult.to_csv("GINI_INDEX_out.csv")
 elif(option == "17"):
-    # Feature selection t_score
-elif(option == "18"):
     # Feature selection SPEC
-elif(option == "19"):
+    X=np.array(feature_o)
+    y=np.asarray(label)
+    y = y[:, 0] 
+    # specify the second ranking function which uses all except the 1st eigenvalue
+    kwargs = {'style': 0}
+    # obtain the scores of features
+    score = spec(X, **kwargs)
+    # sort the feature scores in an descending order according to the feature scores
+    idx = feature_ranking2(score, **kwargs)
+    specresult = feature_o[feature_o.columns[idx[0:selected_number]]]
+    specresult.to_csv("SPEC_out.csv")
+elif(option == "18"):
     # Feature selection Fisher_score
+    X=np.array(feature_o)
+    y=np.asarray(label)
+    y = y[:, 0]
+    score = fisher_score(X, y)
+    # rank features in descending order according to score
+    idx =feature_ranking3(score)
+    fisherresult = feature_o[feature_o.columns[idx[0:selected_number]]]
+    fisherresult.to_csv("Fisher_Score_out.csv")
+elif(option == "19"):
+    # Feature selection t_score
+    X=np.array(feature_o)
+    y=np.asarray(label)
+    y = y[:, 0]
+    score = t_score(X, y)
+    idx =feature_ranking4(score)
+    tscoreresult = feature_o[feature_o.columns[idx[0:selected_number]]]
+    tscoreresult.to_csv("T_Score_out.csv")
 elif(option == "20"):
-    # Feature selection LFDA
-elif(option == "21"):
     # Feature selection IG
     IGresult=IG(feature_o,label,selected_number)
     IGresult.to_csv("IG_out.csv")
