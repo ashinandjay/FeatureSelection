03cb705f5fc24cff2f1b4c8fb1c08d427158bf07
ashinandjay <40191792+ashinandjay@users.noreply.github.com>
2019-07-03 00:32:00 -0500

    Update Feature_Clustering.py

--- Feature_Clustering.py
+++ Feature_Clustering.py
@@ -6,6 +6,8 @@
 import sys,os
 import pandas as pd
 import numpy as np
+from sklearn.cluster import KMeans
+from sklearn.manifold import TSNE
 from sklearn.decomposition import PCA
 from sklearn.decomposition import KernelPCA
 from sklearn.manifold import LocallyLinearEmbedding
@@ -14,7 +16,9 @@ from sklearn.decomposition import TruncatedSVD
 from sklearn.manifold import MDS
 from sklearn.decomposition import FactorAnalysis
 from sklearn.decomposition import FastICA
-from sklearn.cluster import KMeans
+from sklearn.cluster import FeatureAgglomeration
+from sklearn.random_projection import GaussianRandomProjection
+from sklearn.random_projection import SparseRandomProjection
 
 # find the path
 Script_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
@@ -28,6 +32,20 @@ selected_number = sys.argv[2]
 # read feature vectors
 feature = sys.argv[3]
 feature_o = pd.read_csv(feature,delim_whitespace=True,index_col=0,float_precision='round_trip')
+
+# Feature selection Kmeans
+def kmeansselection(k,X):
+    X=np.array(X)
+    kmeansresult=KMeans(n_clusters = k).fit_transform(X)
+    np.savetxt("KMeans_out.csv", kmeansresult, delimiter=",")    
+    return None   
+
+# Feature selection tsne
+def tsneselection(k,X):
+    X=np.array(X)
+    Tsneresult =TSNE(n_components=k).fit_transform(X)
+    np.savetxt("TSNE_out.csv", Tsneresult, delimiter=",")    
+    return None 
 
 # Feature selection PCA
 def pcaselection(k,X):
@@ -89,44 +107,70 @@ def FA(data,n_components):
     np.savetxt("FA_out.csv", FAresult, delimiter=",")
     return None
 
-# Feature selection Kmeans
-def kmeansselection(k,X):
+# Feature selection Agglomerate features
+def Aggselection(k,X):
     X=np.array(X)
-    kmeansresult=KMeans(n_clusters = k).fit_transform(X)
-    np.savetxt("KMeans_out.csv", kmeansresult, delimiter=",")    
+    Aggresult =FeatureAgglomeration(n_clusters=k).fit_transform(X)
+    np.savetxt("Agglomeration_out.csv", Aggresult, delimiter=",")    
     return None 
+
+# Feature selection Gaussian random projection
+def Gaussianselection(k,X):
+    X=np.array(X)
+    gaussiansresult=GaussianRandomProjection(n_components=k).fit_transform(X)
+    np.savetxt("Gaussian_out.csv", gaussiansresult, delimiter=",")
+    return None
+
+# Feature selection Sparse random projection
+def Sparseselection(k,X):
+    X=np.array(X)
+    sparsesresult=SparseRandomProjection(n_components=k).fit_transform(X)
+    np.savetxt("Sparse_out.csv", sparsesresult, delimiter=",")
+    return None
 
 # main code
 
 if(option == "1"):
+    # Feature selection Kmeans
+    kmeansselection(selected_number,feature_o)
+elif(option == "2"):
+    # Feature selection T-SNE
+    tsneselection(selected_number,feature_o)
+elif(option == "3")
     # Feature selection PCA
     pcaselection(selected_number,feature_o)
-elif(option == "2"):
+elif(option == "4"):
     # Feature selection KernelPCA
     kernelPCA(selected_number,feature_o)
-elif(option == "3"):
+elif(option == "5"):
     # Feature selection LLE
     lle(selected_number,feature_o)
-elif(option == "4"):
+elif(option == "6"):
     # Feature selection SVD
     svd(selected_number,feature_o)
-elif(option == "5"):
+elif(option == "7"):
     # Feature selection NMF
     nmf(selected_number,feature_o)
-elif(option == "6"):
+elif(option == "8"):
     # Feature selection MDS
     mds(selected_number,feature_o)
-elif(option == "7"):
+elif(option == "9"):
     # Feature selection ICA
     X=np.array(feature_o)
     ICA(X,n_components=selected_number)
-elif(option == "8"):
+elif(option == "10"):
     # Feature selection FA
     X=np.array(feature_o)
     FA(X,n_components=selected_number)
-elif(option == "9"):
-    # Feature selection KMeans
-    kmeansselection(selected_number,feature_o)
+elif(option == "11"):
+    # Feature selection Agglomerate feature
+    Aggselection(selected_number,feature_o)
+elif(option == "12"):
+    # Feature selection Gaussian Random Projection
+    Gaussianselection(selected_number,feature_o)
+elif(option == "13"):
+    # Feature selection Sparse Random Projection
+    Sparseselection(selected_number,feature_o)
 else:
     print("Invalid method number. Please check the method table!")
 
